208ea3dbef02c6dd17d37e113d95849337982453
smritisruti9 <85470335+smritisruti9@users.noreply.github.com>
2024-06-07 22:30:18 +0530

    Add files via upload

---
+++ Loader.js
@@ -0,0 +1,24 @@
+import React,{useEffect,useState} from 'react'
+import HashLoader from "react-spinners/HashLoader";
+
+function Loader() {
+    let [loading, setLoading] = useState(true);
+    
+  return (
+    <div>
+        <div className="sweet-loading text-center">
+      
+      <HashLoader
+        color='#000'
+        loading={loading}
+        cssOverride=''
+        size={80}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
+    </div>
+    </div>
+  )
+}
+
+export default Loader
